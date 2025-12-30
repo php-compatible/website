76a087d001d2dc8b0f132d6edc7eb27a99ca4131
Daniel Samson <12231216+daniel-samson@users.noreply.github.com>
2025-12-30 14:15:51 +0000

    Update hero text with mission statement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,14 +63,15 @@ function PackagesTable() {
 }
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          Mission
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">
+          To help give developers an automated upgrade path, so that old PHP projects can be upgraded to the latest PHP standards with ease.
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
